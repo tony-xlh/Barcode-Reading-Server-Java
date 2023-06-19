a2c705a5c751886bfca83d8bf1dbc719af5fcbad
xulihang <xulihanghai@163.com>
2023-06-19 16:44:09 +0800

    implement the command line version

--- src/main/java/barcodereader/App.java
+++ src/main/java/barcodereader/App.java
@@ -1,10 +1,28 @@
 package barcodereader;
 
+import com.dynamsoft.dbr.*;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        System.out.println("Started");
+		if (args.length != 1) {
+			System.out.println("Usage: path");
+		    System.exit(1);
+	    }
+		try {
+			String path = args[0];
+			BarcodeReader.initLicense("DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==");
+			BarcodeReader reader = new BarcodeReader();
+			TextResult[] results = reader.decodeFile(path, "");
+			for (TextResult result:results) {
+				System.out.println(result.barcodeFormatString+": "+result.barcodeText);
+			}
+		} catch (BarcodeReaderException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(1);
+		}
+		System.exit(0);
 	}
 
 }
